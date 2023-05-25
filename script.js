31377377f5c15bcee0c3c79284e2d8120b3dd356
bharico <134579872+bharico@users.noreply.github.com>
2023-05-25 17:31:53 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function hitungLuas() {
+
+    var sisiLuas = document.getElementById("sisi-luas").value;
+
+    var style = document.getElementsByClassName("hasil-luas").style.visibility="visible"
+    console.log(style)
+    document.getElementById("output-luas").innerText = <span>sisiLuas</span>;
+}
